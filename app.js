aac4d300b92dd87286a11a7600382582c6e0dac8
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-08-01 21:54:46 +0530

    bcrypt

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ require('dotenv').config()
 const express = require("express");
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require("md5");
+const bcrypt = require('bcrypt');
 
+const saltRounds = 10;
 const app = express();
 
 app.use(express.static('public'));
@@ -41,19 +42,21 @@ app.get("/register",(req,res)=>{
 
 app.post("/register",(req,res)=>{
 
-    const newUser = new User({
-        username:req.body.username,
-        password:md5(req.body.password)
-    })
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            username:req.body.username,
+            password:hash
+        });
 
-    newUser.save((error)=>{
-        if(error){
-            res.send(error);
-        }else{
-            res.render('secrets');
-        }
-    })
+        newUser.save((error)=>{
+            if(error){
+                res.send(error);
+            }else{
+                res.render('secrets');
+            }
+        });
 
+    });
 });
 
 
@@ -65,7 +68,9 @@ app.get("/login",(req,res)=>{
 app.post('/login',(req,res)=>{
 
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
+
+    
 
     if(username && password){
         User.findOne({username:username},(error,foundUser)=>{
@@ -73,11 +78,14 @@ app.post('/login',(req,res)=>{
                 res.send(error);
             }else{
                 if(foundUser){
-                    if(password === foundUser.password){
-                        res.render('secrets');
-                    }else{
-                        res.send("wrong password");
-                    }
+                    bcrypt.compare(password, foundUser.password, function(err, result) {
+                        // result == true
+                        if(result == true){
+                            res.render('secrets')
+                        }else{
+                            res.send("wrong password");
+                        }
+                    });
                 }else{
                     res.send("no user");
                 }
